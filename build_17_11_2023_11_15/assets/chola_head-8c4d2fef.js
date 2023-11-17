b62d236edc56438d0e54c883d198da8aacb88ba5
bharath-sas <bharathkumar@stoneagesolutions.com>
2023-11-17 11:16:42 +0530

    Add a template for remarks with Build

---
+++ build_17_11_2023_11_15/assets/chola_head-8c4d2fef.js
@@ -0,0 +1,1 @@
+const a="/ekycadmin/assets/chola_head-505ae714.png";export{a as c};
---
+++ build_17_11_2023_11_15/assets/reconcellation-9fcb682d.js
@@ -0,0 +1,1 @@
+import{_ as c,o as e,c as o}from"./index-34d95abe.js";const n={},t={class:"p-3"};function r(s,a){return e(),o("div",t,"reconcellation")}const l=c(n,[["render",r]]);export{l as default};
