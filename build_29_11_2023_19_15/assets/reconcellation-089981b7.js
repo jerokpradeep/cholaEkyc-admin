eb0a9f21f6643fd131c46e0a02d891d30d1e2e46
bharath-sas <bharathkumar@stoneagesolutions.com>
2023-11-29 19:14:11 +0530

    connect a generic url and ckyc download with Build

---
+++ build_29_11_2023_19_15/assets/reconcellation-089981b7.js
@@ -0,0 +1,1 @@
+import{_ as c,o as e,c as o}from"./index-15629e2b.js";const n={},t={class:"p-3"};function r(s,a){return e(),o("div",t,"reconcellation")}const l=c(n,[["render",r]]);export{l as default};
