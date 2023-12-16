ae3860c1f246ab90dc436a539a62098c0e3699b6
senthil-sas <senthilnathan@stoneagesolutions.com>
2023-12-16 14:11:35 +0530

    lc

--- src/views/approve-panel/details.vue
+++ src/views/approve-panel/details.vue
@@ -90,8 +90,7 @@ export default {
         this.startDate = window.formatDate(this.cutomerData?.opportunity_data?.creation,'D&T')
         this.updatedOn = window.formatDate(this.cutomerData?.opportunity_data?.modified,'D&T')
         this.modeOfApplication = this.cutomerData?.opportunity_data?.fsl_mode_of_application || this.cutomerData?.opportunity_data?.fsl_modeofapplication
-        // this.branch = this.cutomerData?.opportunity_data?.fsl_branch
-        
+        // this.branch = this.cutomerData?.opportunity_data?.fsl_branch  
     },
 }
 </script>
