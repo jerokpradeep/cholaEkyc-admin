6d6fbd83e81c2546b1bfa835471a534d7e865157
bharath-sas <bharathkumar@stoneagesolutions.com>
2024-01-04 21:00:17 +0530

    do changes as rquested by pradeep with Build

---
+++ build_04_01_2024_21_00_Live/assets/dashboard-5c4db1ff.js
@@ -0,0 +1,1 @@
+import{_ as c,o as e,c as s}from"./index-80da52d8.js";const o={},t={class:"p-3"};function a(r,n){return e(),s("div",t,"das")}const d=c(o,[["render",a]]);export{d as default};
---
+++ build_04_01_2024_21_00_Live/assets/reconcellation-3f69beea.js
@@ -0,0 +1,1 @@
+import{_ as c,o as e,c as o}from"./index-80da52d8.js";const n={},t={class:"p-3"};function r(s,a){return e(),o("div",t,"reconcellation")}const l=c(n,[["render",r]]);export{l as default};
